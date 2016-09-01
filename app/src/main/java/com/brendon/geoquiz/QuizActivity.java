2ea90fa247d5d48fc571d420b2c8d5ec5766676b
Brendon <brennon.mckeever@gmail.com>
2016-09-01 16:43:43 -0500

    Geo Quiz Ch.2 WIP

--- app/src/main/java/com/brendon/geoquiz/QuizActivity.java
+++ app/src/main/java/com/brendon/geoquiz/QuizActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
@@ -15,10 +16,35 @@ public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private int mCurrentIndex = 0;
+
+
+    /*
+    Update questions at the start and whenever the next button is pressed.
      */
+    private void updateQuestion() {
+
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    /*
+    This is the array of questions for the User to answer.
+    */
+    private Question[] mQuestionBank = new Question[] {
+
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+
+    };
+
+
+
     private GoogleApiClient client;
 
 
@@ -27,29 +53,56 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        mTrueButton = (Button) findViewById(R.id.true_button);
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        /*
+        True button Listener
+         */
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, // This is the wrong answer, Constantinople
-                                                                            // or Istanbul is the largest city in Turkey.
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, // I fixed these buttons from Chapter 1.
+
                         Toast.LENGTH_SHORT).show();
             }
         });
 
 
+        /*
+        False button listener
+         */
         mFalseButton = (Button) findViewById(R.id.false_button);
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, // Wrong answer
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast,
                         Toast.LENGTH_SHORT).show();
             }
         });
 
+        /*
+        Next button Listener
+         */
 
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
+
+        /*
+        All of these sections were auto populated when I auto-imported API's. I'm not a fan of this,
+        I don'e like having code I didn't write in my program.
+         */
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
